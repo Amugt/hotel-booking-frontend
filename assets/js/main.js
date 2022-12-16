d3015b1144ffd3eaa783eac6027635ba7348a6c5
Amu <Amansis.g2@gmail.com>
2022-12-16 23:41:04 +0300

    hotel photosstyled

--- assets/js/main.js
+++ assets/js/main.js
@@ -224,13 +224,3 @@
   /**
    * Animation on scroll
    */
-  window.addEventListener('load', () => {
-    AOS.init({
-      duration: 1000,
-      easing: "ease-in-out",
-      once: true,
-      mirror: false
-    });
-  });
-
-})()
